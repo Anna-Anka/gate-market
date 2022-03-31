8435e1f91db2e278a8c5ce4980bba3a3c44f4a3e
Anna-Anka <anjbekrenewa@gmail.com>
2022-03-31 16:38:43 +0300

    added home page desktop

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,9 @@
 $(function(){
-
+    $('.for__items').masonry({
+        // options
+        itemSelector: '.for__item',
+        columnWidth: 305,
+        gutter: 20,
+        horizontalOrder: true,
+    });
 });
