16a44c8ca7fddfc41709e178ae2be967c50ea67a
Anna-Anka <anjbekrenewa@gmail.com>
2022-04-03 20:08:16 +0300

    adaptive tariffs page

--- app/js/main.js
+++ app/js/main.js
@@ -7,32 +7,18 @@ $(function () {
         horizontalOrder: true,
     });
 
-    const btn = document.querySelector('.tariffs__btn');
-    const modal = document.querySelector('.accept');
-    const toggleMenu = function () {
-        modal.classList.toggle('accept--active');
-        document.querySelector('.wrapper').classList.add('lock');
-    }
-
-    btn.addEventListener('click', function (e) {
-        e.stopPropagation();
-        toggleMenu();
+    $('.tariffs__btn').on('click', function () {
+        $('.accept').addClass('accept--active');
+        $('.wrapper').addClass('lock');
     });
 
-    document.addEventListener('click', function (e) {
-        const target = e.target;
-        const its_menu = target == modal || modal.contains(target);
-        const its_btnMenu = target == btn;
-        const menu_is_active = modal.classList.contains('accept--active');
-
-        if (!its_menu && !its_btnMenu && menu_is_active) {
-            toggleMenu();
-            document.querySelector('.wrapper').classList.remove('lock');
+    $(document).on('mouseup', function (e) {
+        var modal = $('.accept, .accept--active');
+        if (!modal.is(e.target) 
+            &&
+            modal.has(e.target).length === 0) { 
+            modal.removeClass('accept--active');
+            $('.wrapper').removeClass('lock');
         }
-    });
-
-    $('.accept__close').on('click', function () {
-        $('.accept').removeClass('accept--active');
-        $('.wrapper').removeClass('lock');
     });
 });
