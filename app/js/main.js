25ac5cd1c86e219cbc3616666a7f9ed86dae8647
Anna-Anka <anjbekrenewa@gmail.com>
2022-04-05 10:58:23 +0300

    adaptive home page

--- app/js/main.js
+++ app/js/main.js
@@ -1,11 +1,29 @@
 $(function () {
+
+    var w = $(window).width();
+
+    if (w <= 768) {
+        $('.for__item:nth-child(3)').before($('.for__item:nth-child(8)'));
+        $('.for__item:nth-child(7)').before($('.for__item:nth-child(5)'));
+        $('.for__item:nth-child(5)').after($('.for__item:nth-child(4)'));
+        $('.for__item:nth-child(5)').before($('.for__item:nth-child(6)'));
+        $('.for__item:nth-child(7)').after($('.for__item:nth-child(6)'));
+        $('.for__item:nth-child(8)').after($('.for__item:nth-child(7)'));
+    }
+
+    
+
     $('.for__items').masonry({
-        // options
         itemSelector: '.for__item',
-        columnWidth: 305,
         gutter: 20,
-        horizontalOrder: true,
+        columnWidth: '.for__item',
+        percentPosition: true,
+        horizontalOrder: true
+    }).imagesLoaded(function () {
+        $('.for__items').masonry('reload');
     });
+
+
 
     $('.tariffs__btn').on('click', function () {
         $('.accept').addClass('accept--active');
@@ -14,9 +32,8 @@ $(function () {
 
     $(document).on('mouseup', function (e) {
         var modal = $('.accept, .accept--active');
-        if (!modal.is(e.target) 
-            &&
-            modal.has(e.target).length === 0) { 
+        if (!modal.is(e.target) &&
+            modal.has(e.target).length === 0) {
             modal.removeClass('accept--active');
             $('.wrapper').removeClass('lock');
         }
